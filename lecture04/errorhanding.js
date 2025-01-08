6bd291652124fee39653c4d1e581d32ec642e353
SirawatVry <s6606021630061@email.kmutnb.ac.th>
2025-01-08 10:21:24 +0700

    fin

---
+++ lecture04/errorhanding.js
@@ -0,0 +1,32 @@
+function criticalcode(){
+    throw "throwing an exception";
+}
+function logError(theException){
+    console.log(theException);
+}
+console.log("\n*******Try..Catch*******\n");
+try{
+    criticalcode();
+}catch (ex){
+    console.log("Got an error");
+    logError(ex);
+}
+console.log("\n*******Throwing in Try..Catch*******\n")
+try {
+    throw"An exception that is thrown every time";
+}catch(ex){
+    console.log("Gat an error");
+    logError(ex);
+}
+console.log("\n*******Try..Catch..Finally*******\n");
+try{
+    criticalcode(ex);
+}   catch(ex){
+        console.log("Got an error");
+        logError(ex);
+    }finally{
+        console.log("code that always will run");
+    }
+    function hello(){
+        console.log("\n*******Throwing Exceptions*******\n")
+}
